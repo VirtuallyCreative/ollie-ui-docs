51c7684d28dfd6826fea2690f845c0229a7d875b
Al Romano <alexander@virtuallycreative.ca>
2020-02-27 19:02:54 -0500

    Swapped Lato for Fira Sans (preference)

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import {Helmet} from 'react-helmet';
+import { Helmet } from 'react-helmet';
 import _ from 'lodash';
 
-import {safePrefix} from '../utils';
+import { safePrefix } from '../utils';
 import Header from './Header';
 import Footer from './Footer';
 
@@ -12,18 +12,18 @@ export default class Body extends React.Component {
             <React.Fragment>
                 <Helmet>
                     <title>{_.get(this.props, 'pageContext.frontmatter.title') && _.get(this.props, 'pageContext.frontmatter.title') + ' - '}{_.get(this.props, 'pageContext.site.siteMetadata.title')}</title>
-                    <meta charSet="utf-8"/>
+                    <meta charSet="utf-8" />
                     <meta name="viewport" content="width=device-width, initialScale=1.0" />
                     <meta name="google" content="notranslate" />
-                    <link href="https://fonts.googleapis.com/css?family=Lato:400,400i,700,700i%26Display=swap" rel="stylesheet"/> 
-                    <link rel="stylesheet" href={safePrefix('assets/css/main.css')}/>
+                    <link href="https://fonts.googleapis.com/css?family=Fira+Sans:400,400i,700,700i&display=swap" rel="stylesheet" />
+                    <link rel="stylesheet" href={safePrefix('assets/css/main.css')} />
                 </Helmet>
                 <div id="page" className="site">
-                  <Header {...this.props} />
-                  <main id="content" className="site-content">
-                    {this.props.children}
-                  </main>
-                  <Footer {...this.props} />
+                    <Header {...this.props} />
+                    <main id="content" className="site-content">
+                        {this.props.children}
+                    </main>
+                    <Footer {...this.props} />
                 </div>
             </React.Fragment>
         );
